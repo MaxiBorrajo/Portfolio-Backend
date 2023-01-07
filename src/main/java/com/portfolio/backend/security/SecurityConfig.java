aa64ee3461ead63582518d49cd4908556eaca1ef
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2023-01-07 15:47:54 -0300

    probando cosas

--- src/main/java/com/portfolio/backend/security/SecurityConfig.java
+++ src/main/java/com/portfolio/backend/security/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  *
@@ -72,5 +74,10 @@ public class SecurityConfig{
     PasswordEncoder passwordEncoder(){
         /*Retorna un encriptador de contraseñas, puesto como componente de spring*/
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
