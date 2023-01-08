b0573d99578242ce196a7ed7d7006ec26bc9358b
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2023-01-08 16:26:02 -0300

    cors

--- src/main/java/com/portfolio/backend/PortfolioBackend.java
+++ src/main/java/com/portfolio/backend/PortfolioBackend.java
@@ -2,6 +2,9 @@ package com.portfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioBackend {
@@ -9,5 +12,14 @@ public class PortfolioBackend {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackend.class, args);
 	}
-
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer(){
+                @Override
+                public void addCorsMappings(CorsRegistry registry){
+                    registry.addMapping("/**").allowedOrigins("https://my-portfolio-6385d.firebaseapp.com");
+                }
+            };
+        }
 }
